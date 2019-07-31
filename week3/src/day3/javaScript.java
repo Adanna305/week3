001b93a416f251dea18e5bc7fba1a7e1368914ec
Adanna305 <52667382+Adanna305@users.noreply.github.com>
2019-07-31 13:25:41 -0400

    Plan A Date
    Date ideas and prices based off budget.

--- week3/src/day3/javaScript.java
+++ week3/src/day3/javaScript.java
@@ -13,19 +13,175 @@ public class javaScript {
 	String name = "";
 	int budget = 20; //integer
 	
-	System.out.println("Hello, welcome to Budget Help");
+	System.out.println("Hello, welcome to Plan A Date.");
 	
 	System.out.println("What's your name?");
 	name = thug.next();
 	
-	System.out.println("Hi " + name + " ,I'm here to help you budget.");
 	
-	System.out.println("So tell me your budget and I'll help you determine");
 	
-	System.out.println("whether you should do indoor or outdoor things.");
+	System.out.println("Hi " + name + " , need help planning a date");
+	System.out.println("and don't know where to go on your budget?");
+	System.out.println("Plan A Date is here to help with that.");
+	System.out.println("What is your budget?");
 	budget = thug.nextInt();
+	 
+	if(budget < 100) {
+		//body
+		
 	
 	
+	System.out.println("Alright, " + budget + " dollars is not bad");
+	System.out.println("I have several options for you.");
+	
+	System.out.println("");
+	
+	System.out.println("Outdoor date ideas?");
+	
+	System.out.println("");
+	
+	System.out.println("Take a stroll downtown river walk (free)");
+	System.out.println("");
+	
+	System.out.println("Museum (most are free)");
+	System.out.println("");
+	
+	System.out.println("Go to the park");
+	System.out.println("");
+	
+	System.out.println("Movie theather (best to go on Tuesday's");
+	System.out.println("for $5 tickets)");
+	System.out.println("");
+	
+	System.out.println("Bowling ($15 a game per person)");
+	System.out.println("");
+	
+	System.out.println("Laser tag ($20 a session per person");
+	System.out.println("");
+	
+	System.out.println("Arcade (100 gameplay for $20)");
+	System.out.println("");
+	
+	System.out.println("Dinner date at buffets like Mongolian Buffet");
+	System.out.println("and Golden Corral are");
+	System.out.println("about $15-$25 for all-you-can-eat"); 
+	
+	System.out.println("");
+	
+	System.out.println("Now here are some indoor date ideas");
+	
+	System.out.println("");
+	
+	System.out.println("Netflix and Chill");
+	System.out.println("(If you don't have an account ask a friend)");
+	System.out.println("");
+	
+	System.out.println("Rent a movie for $1 to $5");
+	System.out.println("");
+	
+	System.out.println("Cook dinner for them ($10 to $60)");
+	System.out.println("");
+	
+	System.out.println("Game night ($5 to $35 depending on game)");
+	System.out.println("");
+	
+	System.out.println("Hopefully this help you plan a perfect date.");
+	System.out.println("Enjoy your date " + name);
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	else if (budget >= 100) {
+		//body
+	
+	System.out.println("Wow " + name + ", there's a lot you can do" );
+	System.out.println("with " + budget + " dollars.");
+	
+	System.out.println("");
+	
+	System.out.println("Museum (most are free)");
+	System.out.println("");
+	
+	System.out.println("Here's some outdoor dating ideas for you");
+	System.out.println("");
+	
+	System.out.println("Movies");
+	System.out.println("");
+	
+	System.out.println("Paintball $15 to $30 per person and session");
+	System.out.println("");
+	
+	System.out.println("Cj Barrymore's $42 wrist band per person");
+	System.out.println("");
+	
+	System.out.println("Nonstop prices can range from $30 to $60");
+	System.out.println("");
+	
+	System.out.println("You can go to Golden Corral, Red Lobster,");
+	System.out.println("Red Robin, Mongolian Buffet, Starter's Grill,");
+	System.out.println("Mario's Restaurant, Olive Garden, etc.");
+	System.out.println("Prices for these restaurants can range");
+	System.out.println("from $40-$120 for the total bill costs.");
+	System.out.println("");
+	
+	System.out.println("Concert tickets range from $40 to $400");
+	System.out.println("");
+	
+	
+	System.out.println("Jet ski riding can range from $45 to $160");
+	System.out.println("depending on the rental time");
+	System.out.println("");
+	
+	System.out.println("Princess Boat Cruise tickets $60 per person");
+	System.out.println("");
+	
+	System.out.println("Now here are some cheap indoor date ideas");
+	System.out.println("just incase you want to do a little saving");
+	System.out.println("");
+	
+	System.out.println("Rent a movie for $1 to $5");
+	System.out.println("");
+	
+	System.out.println("Cook dinner for them ($10 to $60)");
+	System.out.println("");
+	
+	System.out.println("Game night ($5 to $35 depending on game)");
+	System.out.println("");
+	
+	System.out.println("Hopefully this help you plan a perfect date.");
+	System.out.println("Enjoy your date " + name);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
+
 	
 	
 	
