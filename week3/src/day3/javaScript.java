85affadb2c1a1e24370504bc2659e7ce3a2a4903
Adanna305 <52667382+Adanna305@users.noreply.github.com>
2019-07-31 10:38:31 -0400

    Add files via upload

---
+++ week3/src/day3/javaScript.java
@@ -0,0 +1,50 @@
+package day3;
+
+import java.util.Scanner;
+
+public class javaScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner thug = new Scanner (System.in);
+	
+	long money = 4000;
+	String name = "";
+	int budget = 20; //integer
+	
+	System.out.println("Hello, welcome to Budget Help");
+	
+	System.out.println("What's your name?");
+	name = thug.next();
+	
+	System.out.println("Hi " + name + " ,I'm here to help you budget.");
+	
+	System.out.println("So tell me your budget and I'll help you determine");
+	
+	System.out.println("whether you should do indoor or outdoor things.");
+	budget = thug.nextInt();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
