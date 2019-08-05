a84cda93c403562fb5a60f7267599a7ab73e4a33
Adanna305 <52667382+Adanna305@users.noreply.github.com>
2019-08-05 11:39:49 -0400

    Project with Steph

---
+++ daySomething/lastDay.java
@@ -0,0 +1,25 @@
+package daySomething;
+
+public class lastDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+	
+int x = 1;
+for (x=1; x <= 100; x +=2) {
+	System.out.println(x);
+}
+
+
+	
+		
+		
+		
+		
+		
+	}
+
+}
